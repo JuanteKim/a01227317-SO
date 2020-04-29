4c38b8a446f71cd98d0006c70d5fee9367d85835
JuanteKim <a01227317@itesm.mx>
2020-04-29 02:24:23 +0000

    EXAMEN P2

--- segundoparcial/mish.c
+++ segundoparcial/mish.c
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+#include<sys/wait.h> 
  
 #define MAX_LINE 80
 #define MAX_NUM_ARGS 10
  
-size_t string_parser(const char *input, char *word_array[]) {
+size_t string_parser(char *input, char *word_array[]) {
   size_t n = 0;
   while (*input) {
     while (isspace((unsigned char)*input))
@@ -18,9 +17,11 @@ size_t string_parser(const char *input, char *word_array[]) {
       word_array[n++] = (char *)input;
       while (*input && !isspace((unsigned char)*input))
         ++input;
+      *(input)='\0';
+      ++input;
     }
   }
- 
+  word_array[n]=NULL;
   return n;
 }
  
@@ -37,43 +38,22 @@ int main(void) {
     line[strcspn(line, "\n")] = '\0';
     // Separamos la línea en palabras separadas por espacio
     // y las guardamos en args
-    i = string_parser(line,args); // i contiene el número de argumentos
-    //printf("%d, %s %s\n", i, args[1],line); // Es solo para pruebas borrar cuando esté listo
+    i = string_parser(line,args); // i contiene el número de argumentos    
     
-    /**
-    (1) Crear un proceso hijo
-    (2) El hijo debe ejecutar execvp para ejecutar al comando tecleado
-    (3) Si el programa no existe debe imprimir programa no encontrado
-    (4) El proceso padre debe esperar a que el hijo termine, checar funcion wait
-    (5) Debe ejecutar comandos como ls -l /etc o ps -fea
-    (6) Cuando se teclee exit deberá terminar
-    **/
-
-    pid_t child;
-    child = fork();
-
-    if(child != 0){
-       execvp(args[0], args);
-    }else if(child == 0){
-        waitpid(child, 0, 0);
+    if(strcmp(line , "exit") == 0){     
+        should_run = 0;        
+        printf("EXIT\n");  
+        break;                  
     }
-
-    // for(int i = 0 ; i < MAX_NUM_ARGS + 1 ; i++){
-    //     if(strcmp(args[0], args[i]) == 0){
-            
-    //     }else{
-    //         printf("PROGRAMA NO ENCONTRADO");
-    //     }
-    // }
-
-    if( (strcmp(args[0], "exit")) == 0 ){
-        break;
+    int pid=fork(); 
+    if(pid==0){ 
+            execvp(args[0],args);  
     }
-
-
-
-
-
+    else if(pid<0){  
+        printf("Programa no encontrado");
+    }else{ 
+        wait(NULL);
+    }
     fflush(stdout);
     fflush(stdin);
     line[0] = '\0';
